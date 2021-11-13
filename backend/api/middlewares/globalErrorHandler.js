054eaa95325084c534ac935f82673206c07aea37
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 17:48:43 +0000

    Error handler

--- backend/api/middlewares/globalErrorHandler.js
+++ backend/api/middlewares/globalErrorHandler.js
@@ -9,10 +9,23 @@ const sendErrorDev = (err, res) => {
       stack: err.stack,
    });
 };
+
+const sendErrorProd = (err, res) => {
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+   });
+};
+
 module.exports = (err, req, res, next) => {
    err.statusCode = err.statusCode || 500;
    err.status = err.status || 'error';
 
    logger.info('Error found');
+   if (NODE_ENV === 'development') {
       sendErrorDev(err, res);
+   } 
+   else if (NODE_ENV === 'production') {
+      sendErrorProd(err, res);
+   }
 };
--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 
 
+const globalErrorHandler = require('./api/middlewares/globalErrorHandler');
+
+require('./database/connection');
 
 const { NODE_ENV } = require('./config');
 
