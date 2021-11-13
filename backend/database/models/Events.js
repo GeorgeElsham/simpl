23c0d2b2b204fe9772a1abdbcc07429b49186b0f
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 14:55:36 +0000

    Added events

---
+++ backend/database/models/Events.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('events', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            unique: true,
+        },
+        society_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING(256),
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        location: {
+            type: DataTypes.TEXT,
+        },
+        user: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+    });
+};
