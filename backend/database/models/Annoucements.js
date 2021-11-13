f2e426161fefb1a8bccf71aea57d0ec81d7497d7
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 14:57:05 +0000

    Create Annoucements.js

---
+++ backend/database/models/Annoucements.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('announcements', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            unique: true,
+        },
+        society_id: {
+            type: DataTypes.UUID,
+        },
+        event_id: {
+            type: DataTypes.UUID,
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        user: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+    });
+};
