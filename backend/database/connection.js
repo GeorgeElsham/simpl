3162e5b2dc2f52e4055df5a077567bc349c7f728
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 13:25:27 +0000

    DB Connection

---
+++ backend/database/connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const { NODE_ENV } = require('../config');
+const { logger } = require('../functions/logger');
+const sequelizeConnection = require('./dbconfig');
+
+const sequelize = new Sequelize(sequelizeConnection);
+
+
+const society = require('./models/society')(sequelize, Sequelize.DataTypes);
+
+
+module.exports = { society };
+
+if (NODE_ENV == 'development') {
+    const force = process.argv.includes('--forcesync');
+    const sync = process.argv.includes('--sync');
+    if (sync) {
+        sequelize.sync({ force: force });
+        if (force) logger.alert('All models force synced');
+        else logger.alert('All models synced');
+    }
+}
