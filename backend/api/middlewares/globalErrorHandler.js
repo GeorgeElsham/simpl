d39797c662f82f509771898d1fd404308e57ed9f
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 15:37:48 +0000

    Update globalErrorHandler.js

--- backend/api/middlewares/globalErrorHandler.js
+++ backend/api/middlewares/globalErrorHandler.js
@@ -1,4 +1,18 @@
 const { NODE_ENV } = require('../../config');
 const { logger } = require('../../helpers/logger');
+
+const sendErrorDev = (err, res) => {
+   res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+   });
+};
 module.exports = (err, req, res, next) => {
+   err.statusCode = err.statusCode || 500;
+   err.status = err.status || 'error';
+
+   logger.info('Error found');
+      sendErrorDev(err, res);
 };
