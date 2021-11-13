3c733661398aa0380127b2112ed599e6828e7f04
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 13:40:21 +0000

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const { logger } = require('./helpers/logger');
+const app = require('./app');
+const { PORT } = require('./config');
+const port = PORT || 7000;
+const server = app.listen(port, () => {
+	logger.info(`App is running on http://localhost:${port}`);
+});
+
+// handle Globaly  the unhandle Rejection Error which is  outside the express
+// e.g database connection
+process.on('unhandledRejection', (error) => {
+	// it uses unhandledRejection event
+	// using unhandledRejection event
+	logger.info(' Unhandled Rejection => shutting down..... ');
+	logger.info(error.name, error.message);
+	server.close(() => {
+		process.exit(1); //  emidiatly exists all from all the requests sending OR pending
+	});
+});
+
+process.on('uncaughtException', (error) => {
+	// using uncaughtException event
+	logger.info(' uncaught Exception => shutting down..... ');
+	logger.info(error.name, error.message);
+	process.exit(1); //  emidiatly exists all from all the requests
+});
