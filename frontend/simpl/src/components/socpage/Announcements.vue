a119c3c9cd38e17cad70f4972e2bc3faac8a9208
German <german.nikolishin@gmail.com>
2021-11-13 23:10:34 +0000

    ready for matej

---
+++ frontend/simpl/src/components/socpage/Announcements.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1 class="title">Updates</h1>
+    <div class="announcements" v-for="(u, i) in soc.updates" :key="i">
+      <div class="title">{{ u.title }}</div>
+      <div class="date">{{ u.date }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref, setup} from "vue";
+export default {
+    setup(props) {
+        
+    }
+};
+</script>
+<style scoped>
+.container{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+}
+.announcements {
+
+    display: flex;
+    flex-direction: horizontal;
+    align-content: space-around;
+}
+
+</style>
--- frontend/simpl/src/components/socpage/Society.vue
+++ frontend/simpl/src/components/socpage/Society.vue
@@ -1,28 +1,41 @@
 <template>
-    <navbar :society="soc" />
+  <navbar :society="soc" />
+  <announcements :soceity="soc" />
 </template>
 
 <script>
-import {ref, setup} from 'vue';
-import Navbar from '../socpage/Navbar.vue'
-
+import { ref, setup } from "vue";
+import Navbar from "../socpage/Navbar.vue";
+import Announcements from "../socpage/Announcements.vue"
 
 export default {
-    components: {
-        Navbar
-    },
-    setup(props) {
-        const soc = ref({
-            name: "Some Society"
-        })
+  components: {
+    Navbar,
+    Announcements,
+  },
+  setup(props) {
+    const soc = ref({
+      name: "Some Society",
+      updates: [
+        {
+          title: "some title 1",
+          date: new Date("2017-11-12"),
+        },
+        {
+          title: "some title 2",
+          date: new Date("2017-11-12"),
+        },
+        {
+          title: "some title 3",
+          date: new Date("2017-11-12"),
+        },
+      ],
+    });
 
-        return {soc}
-    }
-
-}
-
+    return { soc };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
