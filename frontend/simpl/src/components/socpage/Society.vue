a66b67e018ae7841ab3046dba503dec792b72e5b
German <german.nikolishin@gmail.com>
2021-11-13 22:04:11 +0000

    nav bar

--- frontend/simpl/src/components/socpage/Society.vue
+++ frontend/simpl/src/components/socpage/Society.vue
@@ -1,11 +1,26 @@
 <template>
-  
+    <navbar :society="soc" />
 </template>
 
 <script>
+import {ref, setup} from 'vue';
+import Navbar from '../socpage/Navbar.vue'
+
+
 export default {
+    components: {
+        Navbar
+    },
+    setup(props) {
+        const soc = ref({
+            name: "Some Society"
+        })
+
+        return {soc}
+    }
 
 }
+
 </script>
 
 <style scoped>
--- frontend/simpl/src/router/index.js
+++ frontend/simpl/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/homepage/Home.vue'
 import Login from '../components/auth/Login.vue'
 import Signup from '../components/auth/Signup.vue'
+import Society from '../components/socpage/Society.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/signup',
     name: 'SignUp',
     component: Signup
+  },
+  {
+    path: '/society',
+    name: 'Society',
+    component: Society
   }
 
 ]
