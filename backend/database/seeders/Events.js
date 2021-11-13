3a3aa28282c01d0b8eb5e0ba141af3ae769e5946
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-13 18:19:26 +0000

    Event seeders and services

--- backend/database/seeders/Events.js
+++ backend/database/seeders/Events.js
@@ -1,7 +1,16 @@
+const { v4 } = require('uuid');
 const { EventService } = require('../services');
+const faker = require('faker');
 
 const EventSeed = () => {
-    EventService.create({ data: 'data' });
+    EventService.create({
+        id: v4(),
+        society_id: v4(),
+        title: faker.lorem.lines(1),
+        description: faker.lorem.sentences(Math.floor(Math.random() * (3 - 1 + 1) + 1)),
+        location: 'faker.address.nearbyGPSCoordinate()',
+        user: v4(),
+    });
 }
 
 module.exports = { EventSeed }
--- backend/database/services/EventService.js
+++ backend/database/services/EventService.js
@@ -1,9 +1,29 @@
 const { Events } = require('../connection')
 
-exports.get = (uuid) => {
-    Events.findOne(uuid);
+exports.get = (uuids) => {
+    if (uuids.length === 0) {
+        return Events.findOne({
+            where: {
+                id: uuids[0]
+            }
+        });
+    }
+    else {
+        return Events.findAll({
+            where: {
+                id: uuids
+            }
+        });
+    }
 }
 
 exports.create = (data) => {
-    Events.create(data);
+    return Events.create({
+        id: data.id,
+        society_id: data.society_id,
+        title: data.title,
+        description: data.description,
+        location: data.location,
+        user: data.user,
+    });
 }
