3d55a185b0367598d0f0b8c23cf0c21857b9e211
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-14 06:30:49 +0000

    Better fake data

--- backend/database/seeders/Events.js
+++ backend/database/seeders/Events.js
@@ -7,7 +7,7 @@ const EventSeed = (EventService) => {
         society_id: v4(),
         title: faker.lorem.lines(1),
         description: faker.lorem.sentences(Math.floor(Math.random() * (3 - 1 + 1) + 1)),
-        location: 'faker.address.nearbyGPSCoordinate()',
+        location: ['Online', 'Zepler', 'B46', 'Common', 'Stags', 'Hobbit', 'SUSU Foyer'][Math.floor(Math.random() * (6 - 0 + 1) + 0)],
         user: v4(),
     });
 }
