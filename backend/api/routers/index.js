f6b3c91e85287172e7355256da5d292e201b00d7
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-14 07:15:52 +0000

    Removed useless shit

--- backend/api/routers/index.js
+++ backend/api/routers/index.js
@@ -3,12 +3,10 @@ const router = express.Router()
 
 
 const auth = require('./auth');
-const users = require('./users');
 const societies = require('./societies');
 
 
 router.use('/auth', auth);
-router.use('/users', users);
 router.use('/societies', societies);
 
 
