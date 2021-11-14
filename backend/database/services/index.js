c4ca3a44bd2e2353f7f6fc16a5b977af14de10e0
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2021-11-14 01:57:25 +0000

    Revert "Feature/cookies"

--- backend/database/services/index.js
+++
@@ -1,6 +0,0 @@
-const AnnouncementService = require('./AnnouncementService');
-const EventService = require('./EventService');
-const SocietyService = require('./SocietyService');
-const UserService = require('./UserService');
-
-module.exports = { AnnouncementService, EventService, SocietyService, UserService }
