9feb20b4d2ce1342a4c4a22275785eb241d36fa0
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-14 03:18:14 +0000

    Society router

--- backend/api/routers/index.js
+++ backend/api/routers/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 
 const auth = require('./auth');
-const users = require('./users')
+const users = require('./users');
+const societies = require('./societies');
 
 
 router.use('/auth', auth);
-router.use('/users', users)
+router.use('/users', users);
+router.use('/societies', societies);
 
 
 module.exports = router;
