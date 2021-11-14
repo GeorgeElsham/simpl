ad2e13b048b785d34bc88b36ee37722072ad9803
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-11-14 08:20:16 +0000

    Better fake data and returns

--- backend/database/seeders/seed.js
+++ backend/database/seeders/seed.js
@@ -10,20 +10,20 @@ exports.seed = async ({ AnnouncementService, EventService, SocietyService, UserS
 
     // Generates 100 users
     const users = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 500; i++) {
         users.push(await UserSeed(UserService));
     }
 
     // Generates 10 Societies
     const societies = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 50; i++) {
         societies.push(await SocietySeed(SocietyService));
     }
 
     // Adds all the users to a random number of societies
     for (const user of users) {
         for (let i = 0; i < Math.floor(Math.random() * (7 - 1 + 1) + 1); i++) {
-            const usersociety = await user.addSociety(societies[Math.floor(Math.random() * (9 - 0 + 1) + 0)]);
+            const usersociety = await user.addSociety(societies[Math.floor(Math.random() * (39 - 0 + 1) + 0)]);
             if (usersociety) { // To stop usersociety being undefined if user has already joined the society
                 await usersociety[0].update({ role: roles[Math.floor(Math.random() * (2 - 0 + 1) + 0)] });
             }
@@ -31,15 +31,18 @@ exports.seed = async ({ AnnouncementService, EventService, SocietyService, UserS
         }
     }
 
-    for (let i = 0; i < 10; i++) {
-        societies.push(await EventSeed(EventService));
+    const events = []
+    for (let i = 0; i < 100; i++) {
+        events.push(await EventSeed(EventService, societies[Math.floor(Math.random() * (39 - 0 + 1) + 0)]));
     }
 
-    for (let i = 0; i < 30; i++) {
-        societies.push(await AnnouncementSeed(
+    const announcements = []
+    for (let i = 0; i < 500; i++) {
+        announcements.push(await AnnouncementSeed(
             AnnouncementService,
             users[Math.floor(Math.random() * (99 - 0 + 1) + 0)],
-            societies[Math.floor(Math.random() * (9 - 0 + 1) + 0)]
+            societies[Math.floor(Math.random() * (39 - 0 + 1) + 0)],
+            events[Math.floor(Math.random() * (80 - 0 + 1) + 0)]
         ));
     }
 
